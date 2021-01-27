d5ca3f8fb309b8bcf5a50e417bd6b9e728683e3a
Ryan Rotman <ryan.rotman@gmail.com>
2021-01-26 21:44:59 -0600

    arithmetic operations

---
+++ index.js
@@ -0,0 +1,22 @@
+// create functions capable of different arithmetic operations
+
+let add = function(num1, num2) {
+    return num1 + num2;
+};
+
+let subtract = function(num1, num2) {
+    return num1 - num2;
+};
+
+let multiply = function(num1, num2) {
+    return num1 * num2;
+}
+
+let divide = function(num1, num2) {
+    return num1 / num2;
+}
+
+console.log(add(8, 2));
+console.log(subtract(10, 5));
+console.log(multiply(5, 2));
+console.log(divide(10, 2));
