0ddaa723e7eeccd82497f88a22e4f0f2a276a3d8
Ryan Rotman <ryan.rotman@gmail.com>
2021-01-26 22:27:44 -0600

    create a function that prints all whole numbers from 1 to a given number

--- index.js
+++ index.js
@@ -24,17 +24,36 @@
 
 // TODO: create a function that determines whether a number is odd or even
 
-let oddOrEven = function(num) {
-    if (num % 2 === 0) {
-        return "even";
-    } else {
-        return "odd";
+// let oddOrEven = function(num) {
+//     if (num % 2 === 0) {
+//         return "even";
+//     } else {
+//         return "odd";
+//     }
+// };
+
+// console.log("1:", oddOrEven(1));
+// console.log("3:", oddOrEven(3));
+// console.log("2:", oddOrEven(2));
+// console.log("4:", oddOrEven(4));
+// console.log("1001:", oddOrEven(1001));
+// console.log("10002:", oddOrEven(10002));
+
+
+// TODO: create a function that prints all whole numbers from 1 to a given number
+
+let logNums = function(num) {
+    for (let i = 1; i <= num; i++) {
+        console.log(i);
     }
 };
 
-console.log("1:", oddOrEven(1));
-console.log("3:", oddOrEven(3));
-console.log("2:", oddOrEven(2));
-console.log("4:", oddOrEven(4));
-console.log("1001:", oddOrEven(1001));
-console.log("10002:", oddOrEven(10002));
+logNums(5);
+console.log("----------");
+logNums(10);
+console.log("----------");
+logNums(20);
+console.log("----------");
+logNums(40);
+console.log("----------");
+logNums(100);
